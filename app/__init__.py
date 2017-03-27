586b219ca44f2705963b25be5826a212f99b4d6d
AmonartRolon <alanetro@gmail.com>
2017-03-26 23:02:45 -0300

    Fix the location of the content in the index template
    Add 404 and 500 error handler routes through the app.errorhandler
    decorator, and add the 404 and 500 error templates.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask import render_template
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 from app.main.views import mod as mainModule
 app.register_blueprint(mainModule)
