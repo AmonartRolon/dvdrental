bebe101a8d0d8ebfe6778f3613b13b3cff3b8162
AmonartRolon <alanetro@gmail.com>
2017-03-29 08:18:24 -0300

    Add support to multiple timezones with momentjs flask extension (flask-moment)

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
 from flask import url_for
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask import render_template
 from app import app
+from datetime import datetime
 
 mod = Blueprint('app', __name__, url_prefix = '/')
 
 @mod.route('/')
 def index():
-    return render_template('/main/index.html') 
+    return render_template('/main/index.html', current_time=datetime.utcnow())
