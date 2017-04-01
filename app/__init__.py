3d618c9451496429411250f54ddf492ca3e65c60
AmonartRolon <alanetro@gmail.com>
2017-04-01 16:47:45 -0300

    Add User and Role models and map it using SQLAlchemy and persist it using psycopg2 to talk to Postgres

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,17 @@ from flask_bootstrap import Bootstrap
 from flask import render_template
 from flask import url_for
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'it will be changed sometime in the future'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://postgres:postgres@localhost:5432/dvdrental'
+app.config['SQLALCHMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
--- manager.py
+++ manager.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-from app import app
+from app import app, db
+from app.auth.models import User, Role
 from flask_script import Manager
+from flask_script import Shell
 
 manager = Manager(app)
+
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Role = Role)
+
+manager.add_command("shell", Shell(make_context = make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
