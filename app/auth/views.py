4375d93f69c15d7656c290d807469b51f9aa933e
AmonartRolon <alanetro@gmail.com>
2017-05-24 12:06:01 -0300

    Create the registration view that uses a token to make the confirm/<token> view secure, create a confirm view that can resend the confirmation link by email, Add unconfirmed view and template, Create a link in the login page to the registration page. All users that are logged in but not confirmed their accounts will be redirected to the unconfirmed view this is possible through before_app_request

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,23 @@
 from flask import render_template, redirect, request, url_for, flash
 from app.auth import auth
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from app.auth.models import User
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
+from ..email import send_email
+from app import db
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect('main.index')
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -20,6 +35,42 @@ def login():
 def logout():
     logout_user()
     flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                    'auth/email/confirm', user = user, token = token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form = form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user = current_user, token = token)
+    flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
 
 @auth.route('/secret')
--- app/films/views.py
+++ app/films/views.py
@@ -3,13 +3,16 @@ from flask import url_for
 from app.films.models import Film
 from app import db
 from . import films
+from flask_login import login_required
 
 @films.route('/browse')
+@login_required
 def browse():
     films = Film.query.all()
     return render_template('films/browse.html', films = films)
 
 @films.route('/<int:id>')
+@login_required
 def films(id):
     film = Film.query.get(id)
     return render_template('films/film.html', film = film)
