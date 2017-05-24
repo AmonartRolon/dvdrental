89694518dd8447f3765530f37e3c83f5150a7b7d
AmonartRolon <alanetro@gmail.com>
2017-05-24 18:21:28 -0300

    Create permissions and functions that will allow us to see if a user can or cannot do an action in the app, Change the anonymous proxy of flask_login to implement these functions

--- app/auth/models.py
+++ app/auth/models.py
@@ -3,6 +3,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from flask_login import AnonymousUserMixin
+
+class Permission:
+    CLASSIFY = 0x01
+    WATCH = 0x02
+    MODERATE = 0x04
+    SHOP_OWNER = 0x08
+    ADMINISTER = 0x80
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -51,9 +59,32 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['DVDRENTAL_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default = True).first()
+
+    def can(self, permissions):
+        return self.role is not None and \
+                (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
     def __repr__(self):
         return '<User {email}>'.format(email = self.email)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+login_manager.anonymous_user = AnonymousUser
